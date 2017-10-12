36ae5771456cb21302a0b56a5ecba2e026ce47bb
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-10-12 15:18:49 -0400

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,4 +1,6 @@
+x = []
+
 def word_count(x, n, str):
-    big_enough = filter(lambda b: str.length > n, x)
-    return big_enough
-    print str.count(sub)
+    filter(lambda b: str.length > n, x)
+    reduce(lambda w, v: w.update([(v, w.get(v, 0)+1)]) or w, x.split(), [])
+    return word_count(x, n, str)
